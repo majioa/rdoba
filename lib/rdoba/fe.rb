519f2c408610cf42578663c1f9541880a007a684
Malo Skrylevo <3aHyga@gmail.com>
2012-09-28 13:27:51 +0400

    Переименована функция fenc в более короткую fe. Ссылка же на старую оставлена

---
+++ lib/rdoba/fe.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby -KU
+
+class String
+  def fe
+    if RUBY_VERSION < '1.9'
+      self
+    else
+      return self unless Encoding.default_internal
+      if self.frozen?
+	self.dup.force_encoding(Encoding.default_internal || 'UTF-8').freeze
+      else
+	self.force_encoding(Encoding.default_internal || 'UTF-8')
+      end
+    end
+  end
+  alias :fenc :fe
+end
+
--- lib/rdoba/fenc.rb
+++ lib/rdoba/fenc.rb
@@ -1,17 +1,1 @@
-#!/usr/bin/ruby -KU
-
-class String
-  def fenc
-    if RUBY_VERSION < '1.9'
-      self
-    else
-      return self unless Encoding.default_internal
-      if self.frozen?
-	self.dup.force_encoding(Encoding.default_internal || 'UTF-8').freeze
-      else
-	self.force_encoding(Encoding.default_internal || 'UTF-8')
-      end
-    end
-  end
-end
-
+lib/rdoba/fe.rb
