fb78de9d9efcea4361cc5ea285d600f50e5a8e13
Malo Skrylevo <majioa@yandex.ru>
2013-09-24 15:37:22 +0400

    Добавлены были нѣкоторыя методы бисера Rdoba, въ частности определенія оси и корневой папки бисера.

--- lib/rdoba.rb
+++ lib/rdoba.rb
@@ -21,3 +21,37 @@ module Kernel
         nil; end ||
       if Modules.include? key
         require "rdoba/#{key}"; end; end; end; end
+
+require 'rbconfig'
+
+module Rdoba
+   def self.gemroot name = nil, path = ''
+      if !gem( name )
+         raise "Invalid gem named as #{name.inspect}" ; end
+      g = Gem::Specification.find_by_name( name )
+      File.join g.full_gem_path, path
+   end
+
+   def self.os
+      @@os ||= (
+         host_os = RbConfig::CONFIG['host_os']
+         case host_os
+         when /(mswin|msys|mingw|cygwin|bccwin|wince|emc)/
+            plat = $1 == 'mswin' && 'native' || $1
+            out = `ver`.encode( 'US-ASCII',
+                  :invalid => :replace, :undef => :replace )
+            if out =~ /\[.* (\d+)\.([\d\.]+)\]/
+               "windows-#{plat}-#{$1 == '5' && 'xp' || 'vista'}-#{$1}.#{$2}"
+            else
+               "windows-#{plat}" ; end
+         when /darwin|mac os/
+            'macosx'
+         when /linux/
+            'linux'
+         when /(solaris|bsd)/
+            "unix-#{$1}"
+         else
+            raise "unknown os: #{host_os.inspect}"
+         end)
+   end ; end
+
