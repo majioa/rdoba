81288addef5935e280616ac9e3096db795e1a715
Malo Skrylevo <majioa@yandex.ru>
2013-10-12 15:21:26 +0400

    Поправленъ бысть кодъ обработки параметровъ рдобы въ методѣ Kernel.rdoba

--- lib/rdoba.rb
+++ lib/rdoba.rb
@@ -1,29 +1,29 @@
 #encoding: utf-8
 
 module Kernel
-  Modules = [ :bcd ]
+   Modules = [ :bcd ]
 
-  def rdoba *options
-    options.each do |option|
-      key, value = ( option.is_a?( Hash ) &&
-      [ ( k = option.keys.shift ).to_s.to_sym, option[ k ] ] ||
-      [ option.to_s.to_sym ] )
-      case key
-      when :mixin
-        require "rdoba/mixin"
-        Rdoba.mixin value
-      when :log, :debug
-        require "rdoba/#{key}"
-        Rdoba.log( { :in => self }.merge( value || {} ) ) # TODO move log method into module
-        # TODO and resolve some troubles with eval in main
-      when :slovo
-        require "rdoba/#{key}"
-        Rdoba.slovo value
-      else
-        Kernel.puts STDERR, "Unknown rdoba module named as '#{key.to_s}'"
-        nil; end ||
-      if Modules.include? key
-        require "rdoba/#{key}"; end; end; end; end
+   def rdoba *options
+      options.each do |option|
+         ( option.is_a?( Hash ) &&
+               option || { option.to_s.to_sym => true }
+         ).each_pair do |key, value|
+            case key
+            when :mixin
+               require "rdoba/mixin"
+               Rdoba.mixin value
+            when :log, :debug
+               require "rdoba/#{key}"
+               Rdoba.log( { :in => self }.merge( value || {} ) ) # TODO move log method into module
+               # TODO and resolve some troubles with eval in main
+            when :slovo
+               require "rdoba/#{key}"
+               Rdoba.slovo value
+            else
+               Kernel.puts STDERR, "Unknown rdoba module named as '#{key.to_s}'"
+               nil; end ||
+            if Modules.include? key
+               require "rdoba/#{key}"; end; end; end; end ; end
 
 require 'rbconfig'
 
