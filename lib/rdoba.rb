e136441143a3665cc1cbb66f4591633192c5316d
Malo Skrylevo <majioa@yandex.ru>
2013-10-13 16:02:27 +0400

    Пределанъ быть загрузчикъ модулей Рдобы

--- lib/rdoba.rb
+++ lib/rdoba.rb
@@ -1,29 +1,17 @@
 #encoding: utf-8
 
 module Kernel
-   Modules = [ :bcd ]
+   Modules = [ :bcd, :mixin, :log, :debug, :slovo ]
 
    def rdoba *options
       options.each do |option|
          ( option.is_a?( Hash ) &&
                option || { option.to_s.to_sym => true }
          ).each_pair do |key, value|
-            case key
-            when :mixin
-               require "rdoba/mixin"
-               Rdoba.mixin value
-            when :log, :debug
-               require "rdoba/#{key}"
-               Rdoba.log( { :in => self }.merge( value || {} ) ) # TODO move log method into module
-               # TODO and resolve some troubles with eval in main
-            when :slovo
-               require "rdoba/#{key}"
-               Rdoba.slovo value
-            else
-               Kernel.puts STDERR, "Unknown rdoba module named as '#{key.to_s}'"
-               nil; end ||
             if Modules.include? key
-               require "rdoba/#{key}"; end; end; end; end ; end
+               require "rdoba/#{key}"
+               if Rdoba.methods.include? key
+                  Rdoba.send key, value ; end ; end ; end; end ; end ; end
 
 require 'rbconfig'
 
--- lib/rdoba/debug.rb
+++ lib/rdoba/debug.rb
@@ -1,6 +1,10 @@
 # encoding: utf-8
 
-STDERR.puts "Warning: the module has kept only for backward compatibility\n" +
+STDERR.puts "Warning: the module has kept only for backward compatibility\n" \
 "Please use 'rdoba :log' form instead"
+
 require 'rdoba/log'
 
+module Rdoba
+   def self.debug options = {}
+      Rdoba.log options ; end ; end
