534210aedc43bce65ba9b8c5d5b373aa5c23fba9
Malo Skrylevo <majioa@yandex.ru>
2013-10-22 14:06:19 +0400

    Подновленъ бысть кутъ окруженꙇя тестофки, да временъ кутъ маꙇєсѧ

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,5 @@
 require 'rdoba'
-require 'tempfile'
+require 'tmpdir'
 require 'open3'
 require 'coveralls'
 require "codeclimate-test-reporter"
@@ -12,7 +12,6 @@ Coveralls.wear!
 CodeClimate::TestReporter.start
 SimpleCov.start
 ##############################################
-
 
 RdobaSimSimpleHead=<<HEAD
 #!/usr/bin/env ruby
@@ -45,7 +44,7 @@ def format str
 def store
   file = @inithead + @init + @echo + ( "end\n" * @deep )
   puts file
-  File.open( 'tmp.rb', 'w+' ) do |f|
+  File.open( @tmpfile, 'w+' ) do |f|
     f.puts file ; end ; end
 
 def match_keywords keystr
@@ -60,6 +59,9 @@ def rdoba_sim sub, cmd, *args
   when :log, :debug
     case cmd
     when :init
+      $tmpdir ||= Dir.mktmpdir
+      @tmpfile ||= File.join( $tmpdir, 'tmp.rb' )
+
       opts = match_keywords args[ 1 ]
       opts = opts.size > 1 && opts || opts.size > 0 && opts[ 0 ] || nil
 
@@ -72,7 +74,7 @@ def rdoba_sim sub, cmd, *args
 
       param = case args[ 0 ]
       when 'io'
-        { :io => "File.new( 'tmp.log', 'w+' )", :functions => :basic }
+        { :io => "File.new( @tmpfile, 'w+' )", :functions => :basic }
       when 'as'
         { :as => ":log", :functions => :basic }
       when 'in'
@@ -148,8 +150,8 @@ def rdoba_sim sub, cmd, *args
         store
         puts '-' * 15
 
-        File.chmod 0755, 'tmp.rb'
-        Open3.popen3( "./tmp.rb" ) do |stdin, stdout, stderr, wait_thr|
+        File.chmod 0755, @tmpfile
+        Open3.popen3( @tmpfile ) do |stdin, stdout, stderr, wait_thr|
           @out = stdout.read
           @err = stderr.read
         end
@@ -157,7 +159,7 @@ def rdoba_sim sub, cmd, *args
       end
       res = case args[ 0 ]
       when :file
-        File.new( 'tmp.log', 'r' ).read
+        File.new( @tmpfile, 'r' ).read
       when :stdout
         @out
       when :stderr
@@ -169,3 +171,4 @@ def rdoba_sim sub, cmd, *args
   end
 end
 
+at_exit { $tmpdir && ( FileUtils.remove_entry_secure $tmpdir ) }
