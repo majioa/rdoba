86df646babdc8e57d30f4e95a670cbc0101c54a2
Malo Skrylevo <majioa@yandex.ru>
2015-08-17 23:25:56 +0300

    Added `merge` module.

---
+++ lib/rdoba/merge.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+module Rdoba::Merge
+   TARGET = :Hash
+
+   def deep_merge source, dest
+      dup = dest.dup
+      source.each do |key, value|
+         newvalue = dup.delete key
+         case newvalue
+         when Hash
+            value.deep_merge newvalue
+         when Array
+            value |= newvalue
+         when NilClass
+         else
+            raise
+         end
+      end
+   end
+end
