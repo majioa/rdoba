f3b5753d82f0b365118d60fd2685f1cc90e5a2cb
Awesome Code <team@xinminlabs.com>
2021-06-23 10:51:04 +0000

    Auto corrected by following Lint Ruby Style/Not

--- lib/rdoba/combinations.rb
+++ lib/rdoba/combinations.rb
@@ -39,7 +39,7 @@ class Array
   public
 
   def each_comby(*args)
-    return self if self.empty? or not block_given?
+    return self if self.empty? or !block_given?
     if args.include?(:backward)
       yield [self.dup]
       ((1 << (self.size - 1)) - 2).downto(0) do |i|
--- lib/rdoba/io.rb
+++ lib/rdoba/io.rb
@@ -15,7 +15,7 @@ module Kernel
     fmt = format.split('%')
     nformat = fmt.shift
 
-    while (not fmt.empty?)
+    while (!fmt.empty?)
       part = fmt.shift
       part = '%' + fmt.shift unless part
       if part =~ /([0-9 #+\-*.]*)([bcdEefGgiopsuXxP])(.*)/ and $2 == 'P'
