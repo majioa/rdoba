2727409d49f4a57d55ccad2c2424dd7abec9d98c
Awesome Code <team@xinminlabs.com>
2021-06-24 07:45:31 +0000

    Auto corrected by following Lint Ruby Style/CommentedKeyword

--- lib/rdoba/log.rb
+++ lib/rdoba/log.rb
@@ -204,7 +204,8 @@ module Rdoba
       end
     end
 
-    module DebugCompat # TODO compat
+# TODO compat
+    module DebugCompat
       def dbgl
         @dbgl
       end
@@ -402,7 +403,7 @@ module Rdoba
     def self.update_functions(functions, obj, method)
       if functions.is_a?(Array) && functions.include?(:*)
         functions = [:basic, :enter, :leave, :warn, :info, :extended, :compat]
-      end # TODO compat
+      end
       cf = self.log_functions_get obj
       functions =
         cf.send(
