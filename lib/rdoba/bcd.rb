66f71025120b962b22aabb9fb56d63f43524bd96
Awesome Code <team@xinminlabs.com>
2021-06-24 09:07:52 +0000

    Auto corrected by following Lint Ruby Redundant

--- lib/rdoba/bcd.rb
+++ lib/rdoba/bcd.rb
@@ -13,7 +13,7 @@ module BCD
   def to_i
     res = 0
     mul = 1
-    self.each_byte do |c|
+    each_byte do |c|
       def pow(value, mul)
         if value >= 10
           raise ConvertError
@@ -64,7 +64,7 @@ class BCDString < String
 
   def initialize(value = nil)
     if value.is_a? Numeric
-      self.replace BCD.parse(value)
+      replace BCD.parse(value)
     else
       super value.to_s
     end
