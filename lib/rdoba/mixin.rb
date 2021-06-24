2d76abfafbda4d709ae282bfacea73e7521b8040
Awesome Code <team@xinminlabs.com>
2021-06-24 09:06:45 +0000

    Auto corrected by following Lint Ruby Style/CommentedKeyword

--- lib/rdoba/mixin.rb
+++ lib/rdoba/mixin.rb
@@ -333,7 +333,7 @@ module Rdoba
         end
         Mixin::CaseString::Fixups.each do |e|
           ::String.class_eval "def #{e}(*args);self.__rdoba_mixin_#{e}__(*args);end"
-        end # trap NameError
+        end
         ::String.send :include, Mixin::CaseString
       when :reverse
         Mixin::ReverseString::Aliases.each do |k, v|
@@ -341,7 +341,7 @@ module Rdoba
         end
         Mixin::ReverseString::Fixups.each do |e|
           ::String.class_eval "def #{e}(*args);self.__rdoba_mixin_#{e}__(*args);end"
-        end # trap NameError
+        end
         String.send :include, Mixin::ReverseString
       when :compare
         String.send :include, Mixin::CompareString
