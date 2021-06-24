17bbdcf9f361dc3c0e04b8cefd8b0117d9387ac2
Awesome Code <team@xinminlabs.com>
2021-06-24 09:40:15 +0000

    Auto corrected by following Lint Ruby Performance/RegexpMatch

--- lib/rdoba/deploy.rb
+++ lib/rdoba/deploy.rb
@@ -13,10 +13,10 @@ class Hash
     res = {}
 
     self.keys.sort do |x, y|
-      if x =~ /=$/
-        y =~ /=$/ ? x <=> y : -1
+      if /=$/.match?(x)
+        /=$/.match?(y) ? x <=> y : -1
       else
-        y =~ /=$/ ? 1 : x <=> y
+        /=$/.match?(y) ? 1 : x <=> y
       end
     end.each do |key|
       if key =~ /(.*)=$/
