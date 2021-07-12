07aed1002b5bfdb16df111663a16f79e293f14dd
Awesome Code <team@xinminlabs.com>
2021-07-12 21:12:59 +0000

    Auto corrected by following Lint Ruby Style/CommentAnnotation

--- lib/rdoba/log.rb
+++ lib/rdoba/log.rb
@@ -201,7 +201,7 @@ module Rdoba
       end
     end
 
-    # TODO compat
+    # TODO: compat
     module DebugCompat
       def dbgl
         @dbgl
@@ -272,7 +272,7 @@ module Rdoba
       def get_stack_function_data_at_level(level)
         raise Exception
       rescue Exception
-        #TODO check match a method containing '`'
+        #TODO: check match a method containing '`'
         $@[level] =~ %r{([^/]+):(\d+):in `(.*?)'$}
         [Regexp.last_match(1), Regexp.last_match(3), Regexp.last_match(2)]
       end
@@ -324,7 +324,7 @@ module Rdoba
       options = Rdoba::Log.class_variable_get :@@options
       io = options[:io] || $stdout
 
-      # TODO puts costomize
+      # TODO: puts costomize
       io_m = io.method :puts
     end
 
