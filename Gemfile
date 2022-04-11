48873186a7e2ab0c6c744b642e7a3c14f0402d7b
Pavel «Malo» Skrylev <majioa@yandex.ru>
2022-04-11 19:49:26 +0300

    refactor: crop log in favor of homon gem
    - crop log in favor of homon gem

--- Gemfile
+++ Gemfile
@@ -5,7 +5,5 @@ source "http://rubygems.org"
 # Specify your gem's dependencies in rdoba.gemspec
 gemspec
 
-group :development do
-   gem "codeclimate-test-reporter", :group => :test, :require => nil ; end
-   gem 'ffi', :github => 'majioa/ffi'
-   gem 'ffi-stat', :github => 'majioa/ffi-stat'
+gem 'ffi', :github => 'majioa/ffi'
+gem 'ffi-stat', :github => 'majioa/ffi-stat'
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,6 @@ require 'rdoba'
 require 'tmpdir'
 require 'open3'
 require 'coveralls'
-require 'codeclimate-test-reporter'
 require 'simplecov'
 require 'rspec/expectations'
 
@@ -12,7 +11,6 @@ require 'rspec/expectations'
 #
 #
 Coveralls.wear!
-CodeClimate::TestReporter.start
 SimpleCov.start
 ##############################################
 
@@ -20,14 +18,6 @@ RdobaSimSimpleHead = <<~HEAD
   #!/usr/bin/env ruby
   
   require 'rdoba'
-HEAD
-
-RdobaCodeClassDeclaration = <<~HEAD
-  class Cls
-    def initialize
-      log > {:variable=>"value"}
-    end
-  end
 HEAD
 
 RdobaSimClsHead = <<~HEAD
@@ -56,143 +46,6 @@ def match_keywords(keystr)
       %i[basic extended enter leave compat timestamp pid function_name function_line function].include?(k.to_sym) &&
         k.to_sym || nil
     end.compact
-end
-
-def rdoba_sim(sub, cmd, *args)
-  echo = nil
-  case sub
-  when :log, :debug
-    case cmd
-    when :init
-      $tmpdir ||= Dir.mktmpdir
-      @tmpfile ||= File.join($tmpdir, 'tmp.log')
-
-      if args[1] == 'class'
-        @deep = 1
-        @inithead = RdobaSimClsHead
-      else
-        @deep = 0
-        @inithead = RdobaSimSimpleHead
-      end
-      @echo = ''
-      @init = ''
-      puts @inithead
-    when :apply
-      opts = match_keywords args[1]
-
-      #      opts = opts.size > 2 && opts || opts.size > 0 && opts[ 1 ] || nil
-      param =
-        case args[0]
-        when 'io'
-          { io: "File.new( File.join( '#{@tmpfile}' ), 'w+' )", functions: :basic }
-        when 'as'
-          { as: ':self', functions: :basic }
-        when 'in'
-          { in: 'Cls', functions: :basic }
-          #      when /in.*as/
-          #        { :in => "Cls", :as => args[ 2 ].to_sym, :functions => :basic }
-        when 'prefix'
-          #        basic|extended|enter|leave|compat
-          { args[0].to_sym => opts, :functions => :basic }
-        when 'functions'
-          { args[0].to_sym => opts }
-        else
-          { functions: :basic }
-        end
-
-      if args[2]
-        param.merge!(as: args[2].to_sym)
-      end
-
-      param =
-        '{' + param.to_a.map do |v| "#{v[0].inspect} => #{v[1].is_a?(String) && v[1] || v[1].inspect}"end.join(',') +
-          '}'
-
-      @echo << '   ' * @deep + "rdoba :#{sub} => #{param}\n"
-    when :func
-      call =
-        case args[0]
-        when 'keyword'
-          'self'
-        when 'invalid keyword'
-          'errlog'
-        when /^db/
-          @echo << "self.dbgl = 0xff\n"
-          args[0]
-        else
-          (args[0] || :log).to_sym
-        end
-
-      sep = (args[1] == :e) && '.' || ' '
-      param = []
-      args[2..-1].each do |arg|
-        case arg
-        when NilClass
-        when Symbol
-          param << " #{arg}"
-        else
-          param << " #{arg.inspect}"
-        end
-      end
-      str = "#{call}#{sep}#{args[1]}#{param.join(',')}"
-      echo = format str
-      @echo << echo
-    when :close
-      while @deep > 0
-        @deep -= 1
-        @echo << (format 'end')
-      end
-    when :declare
-      @echo << (format RdobaCodeClassDeclaration)
-    when :create
-      @echo << (format 'Cls.new')
-    when :call
-      @echo << (format 'Cls.singleton')
-    when :def
-      if @inithead == RdobaSimClsHead
-        @deep = 1
-      else
-        @deep = 0
-      end
-      echo =
-        case args[0]
-        when :init
-          @deep += 1
-          format 'def initialize'
-        when :single
-          @deep += 1
-          format 'def self.singleton'
-        else
-          ''
-        end
-      @echo << echo
-    when :exec
-      unless @echo.empty?
-        store
-        puts '-' * 15
-
-        File.chmod 0o755, @tmpfile
-        Open3.popen3(@tmpfile) do |_stdin, stdout, stderr, _wait_thr|
-          @out = stdout.read
-          @err = stderr.read
-        end
-        @echo = ''
-      end
-      res =
-        case args[0]
-        when :file
-          File.new(@tmpfile, 'r').read
-        when :stdout
-          @out
-        when :stderr
-          @err
-        else
-          return
-        end
-      puts res
-      res
-    end
-  end
 end
 
 at_exit { $tmpdir && (FileUtils.remove_entry_secure $tmpdir) }
